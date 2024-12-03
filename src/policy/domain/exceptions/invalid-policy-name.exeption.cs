6cf5888ae23f2a5da83fd07f5f592ff0bef04559
AntoniobB09 <antblanco@gmail.com>
2024-12-03 13:34:37 -0400

    policy-value-objects

---
+++ src/policy/domain/exceptions/invalid-policy-name.exeption.cs
@@ -0,0 +1,10 @@
+﻿using OrdersMicroservice.Core.Domain;
+namespace OrdersMicroservice.src.policy.domain.exceptions
+{
+    public class InvalidPolicyNameException : DomainException
+{
+    public InvalidPolicyNameException() : base("Invalid policy name")
+    {
+    }
+}
+}
